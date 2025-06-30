ccfb5e4f2038bd6d15561412d08e7f4110c4f270
Ruan <oliveiraruan2018@gmail.com>
2025-06-29 20:15:18 -0400

    refactor: otimize code with prettier

--- EcoWebMobile/src/services/api.ts
+++ EcoWebMobile/src/services/api.ts
@@ -2,10 +2,9 @@ import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const api = axios.create({
-  baseURL: 'https://api-3r74.onrender.com', // seu endpoint base
+  baseURL: 'https://api-3r74.onrender.com',
 });
 
-// Interceptor para adicionar token nas requisições
 api.interceptors.request.use(
   async (config) => {
     const token = await AsyncStorage.getItem('@ecoweb_token');
